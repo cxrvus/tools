d6b430cbc1eedcf4f6666bce2843c82682c2d4ec
cxrvus <benito.m.rabe@gmail.com>
2026-08-14 20:18:24 +0200

    fix whitespace removal in bullet

--- md/bullet.js
+++ md/bullet.js
@@ -18,7 +18,7 @@ try {
 	process.exit(1)
 }
 
-fileContent = fileContent.replace(/:\n/, '\n').replace(/\s?\(\[\[.+?\]\]\) ?/g, '')
+fileContent = fileContent.replace(/:\n/, '\n').replace(/\s?\(\[\[.+?\]\]\)/g, '')
 
 const blocks = fileContent.split(/\n{2,}/)
 
